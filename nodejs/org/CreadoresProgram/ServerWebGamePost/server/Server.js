d56846d72783a952bd920cff60bd976aefbd3b51
Trollhunters501PC <cpcreador@gmail.com>
2024-09-12 20:42:49 -0600

    Listo para Pruebas

--- nodejs/org/CreadoresProgram/ServerWebGamePost/server/Server.js
+++ nodejs/org/CreadoresProgram/ServerWebGamePost/server/Server.js
@@ -1,4 +1,6 @@
 const http = require("http");
+const fs = require("fs");
+const path = require("path");
 class Server {
     get port(){
         return this.port;
@@ -34,7 +36,11 @@ class Server {
         this.port = port;
         this.players = {};
         this.imgSrc = imgSrc;
+        this.processDatapacks = procecerDatapacks;
         this.httpServer = http.createServer(this.processSubDatapacks);
+        this.httpServer.listen(this.port, "0.0.0.0", ()=>{
+            return true;
+        });
     }
     sendDataPacket(identifier, datapack){
         if(this.players[identifier] == null){
@@ -43,20 +49,51 @@ class Server {
         this.players[identifier][this.players[identifier].length] = datapack;
     }
     processSubDatapacks(request, reponse){
-        if(request.method != "POST"){
-            reponse.statusCode = 404;
-            return;
-        }
-        if(request.url == "/favicon.ico"){
+        try{
+          if(request.url == "/favicon.ico"){
+            if(request.method != "GET"){
+                reponse.statusCode = 404;
+                return;
+            }
             if(this.imgSrc == null){
                 reponse.end("");
                 return;
             }
-
-        }else if(){
-
-        }else{
-            
+            let logoPath = path.join(__dirname, this.imgSrc);
+            fs.readFile(logoPath, (err, logo)=>{
+                if(err){
+                    reponse.statusCode = 500;
+                    reponse.setHeader('Content-Type', 'text/plain');
+                    reponse.end(err);
+                    return;
+                }
+                reponse.statusCode = 200;
+                reponse.setHeader("Content-Type", "image/jpeg");
+                reponse.end(logo);
+            });
+          }else if(request.url == "/ServerWebGamePost"){
+            if(request.method != "POST"){
+                reponse.statusCode = 404;
+                return;
+            }
+            let body = "";
+            request.on("data", chunk =>{
+                body += chunk.toString();
+            })
+            request.on("end", ()=>{
+                let datapack = JSON.parse(body);
+                this.processDatapacks(datapack);
+                let responDatapacks = {};
+                responDatapacks.datapacksLot = this.players[datapack.identifier];
+                reponse.statusCode = 200;
+                reponse.setHeader("Content-Type", "application/json");
+                reponse.end(JSON.stringify(responDatapacks));
+            });
+          }
+        }catch(erro){
+            reponse.statusCode = 500;
+            reponse.setHeader('Content-Type', 'text/plain');
+            reponse.end(erro);
         }
     }
 }
