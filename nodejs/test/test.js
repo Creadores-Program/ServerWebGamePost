15fb99473f7238f2c965b0bc06fe43f9299e4639
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2024-09-14 14:40:13 -0600

    Update test.js

--- nodejs/test/test.js
+++ nodejs/test/test.js
@@ -1,7 +1,7 @@
 const ServerWebGamePost = require("../index");
 const prefix = "[Creadores Program Test] ";
 console.info(prefix+"Test Server...");
-const helloWoldData = (datapack)=>{
+const helloWoldData = function (datapack){
     console.info((Date.now() - datapack.ping) + "ms Ping");
     ServerWGP.sendDatapacket({
         status: "OK"
@@ -10,9 +10,10 @@ const helloWoldData = (datapack)=>{
     console.info(prefix+"Test Done!");
 };
 var ServerWGP = new ServerWebGamePost.Server(3000, null, helloWoldData);
+ServerWGP.processDatapacks = helloWoldData;
 console.info(prefix+"Test Client...");
 var ClientWGP = new ServerWebGamePost.Client("127.0.0.1", 3000, false);
-const okf = (d)=> {
+const okf = function(d) {
     console.info(d.status);
 }
 ClientWGP.processDatapacks = okf;
