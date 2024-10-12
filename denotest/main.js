794e8c1a1d998243fa5e9d5d18d372edba994d42
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2024-10-12 08:23:58 -0600

    Create main.js

---
+++ denotest/main.js
@@ -0,0 +1,24 @@
+import { createRequire } from "node:module";
+const require = createRequire(import.meta.url);
+const ServerWebGamePost = require("ServerWebGamePost");
+const prefix = "[Creadores Program Test] ";
+console.info(prefix+"Test Server...");
+const helloWoldData = function (datapack){
+    console.info((Date.now() - datapack.ping) + "ms Ping");
+    ServerWGP.sendDataPacket(datapack.identifier, {
+        status: "OK"
+    });
+    ServerWGP.stop();
+    console.info(prefix+"Test Done!");
+};
+var ServerWGP = new ServerWebGamePost.Server(3000, null, helloWoldData);
+console.info(prefix+"Test Client...");
+var ClientWGP = new ServerWebGamePost.Client("127.0.0.1", 3000, false);
+const okf = function(d) {
+    console.info(d.status);
+}
+ClientWGP.processDatapacks = okf;
+ClientWGP.sendDatapacket({
+    identifier: "108023",
+    ping: Date.now()
+});
