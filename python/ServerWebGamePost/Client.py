a6cf90a698882dae519abbbe7dafd63e319b6aa5
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2024-10-19 13:27:46 -0600

    Create Client.py

---
+++ python/ServerWebGamePost/Client.py
@@ -0,0 +1,24 @@
+import requests
+class Client:
+  def __init__(self, domain, port, isHttps):
+    self.domain = domain
+    self.port = port
+    self.isHttps = isHttps
+  def setProcessDatapacks(self, callb):
+    self.processdatapacks = callb
+  def getProcessDatapacks(self):
+    return self.processdatapacks
+  def __processSubDatapackspriv(self, datapacks):
+    for i in datapacks["datapacksLot"]:
+      self.processdatapacks(datapacks["datapacksLot"][i])
+  def sendDataPacket(self, datapack):
+    prefix = ""
+    if(self.isHttps):
+      prefix = "https://"
+    else:
+      prefix = "http://"
+    res = requests.post(prefix+self.domain+":"+self.port+"/ServerWebGamePost", json=datapack).json()
+    self.__processSubDatapackspriv(res)
+    
+    
+    
