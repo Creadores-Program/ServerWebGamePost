d9e858802e1c500ae148edaf586a1462bcc0fbb4
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2024-10-19 08:56:44 -0600

    Create setup.py

---
+++ python/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+setup(
+  name="ServerWebGamePost",
+  version="1.0.0",
+  packages=find_packages(exclude=['tests*']),
+  license="MIT",
+  description="A multiplayer server system based on web servers with POST method",
+  long_description="A multiplayer server system based on web servers with POST method",
+  url="https://github.com/Creadores-Program/ServerWebGamePost",
+  author="Creadores Program"
+)
