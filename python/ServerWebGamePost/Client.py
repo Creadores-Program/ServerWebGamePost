134408aeaa6ecda0fdf6d3433c828d979bf2bdc9
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2024-10-19 22:27:54 -0600

    Update Client.py

--- python/ServerWebGamePost/Client.py
+++ python/ServerWebGamePost/Client.py
@@ -12,11 +12,7 @@ class Client:
     for i in datapacks["datapacksLot"]:
       self.processdatapacks(datapacks["datapacksLot"][i])
   def sendDataPacket(self, datapack):
-    prefix = ""
-    if(self.isHttps):
-      prefix = "https://"
-    else:
-      prefix = "http://"
+    prefix = "https://" if self.isHttps else "http://"
     res = requests.post(prefix+self.domain+":"+self.port+"/ServerWebGamePost", json=datapack).json()
     self.__processSubDatapackspriv(res)
     
