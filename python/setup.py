34cec6d80e005894796f5ae56a83225eaab4f8f0
Trollhunters501PC <cpcreador@gmail.com>
2024-12-15 19:32:38 -0600

    Actualizar version 1.1.0 < 1.1.1

--- python/setup.py
+++ python/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
   name="ServerWebGamePost",
-  version="1.1.0",
+  version="1.1.1",
   packages=find_packages(exclude=['tests*']),
   license="MIT",
   description="A multiplayer server system based on web servers with POST method",
